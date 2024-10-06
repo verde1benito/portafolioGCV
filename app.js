2826b2e8655da5e31f266996d04f32cd03963fff
Gabriela Carmona Vazquez <gabycarmona1409@gmail.com>
2024-10-06 14:41:15 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+//Selección de desplazar hacia arriba
+const scrollUp = document.querySelector("#scroll-up");
+//Funcionalidad de desplazar hacia arriba
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
